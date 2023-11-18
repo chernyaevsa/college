4c69e5903a2e3ddd4044a6eae6cfb14035328576
csa <csa@czsa.ru>
2023-11-18 09:38:53 +0500

    Добавил базу данных.

---
+++ API/Models/CollegeContext.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public partial class CollegeContext : DbContext
+{
+    public CollegeContext()
+    {
+    }
+
+    public CollegeContext(DbContextOptions<CollegeContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Lesson> Lessons { get; set; }
+
+    public virtual DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySQL("Server=localhost;User=root;Database=college");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Lesson>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("lessons");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.CountHours)
+                .HasMaxLength(45)
+                .HasColumnName("count_hours");
+            entity.Property(e => e.Name)
+                .HasMaxLength(45)
+                .HasColumnName("name");
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("students");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DateOfBirth)
+                .HasMaxLength(45)
+                .HasColumnName("date_of_birth");
+            entity.Property(e => e.Group)
+                .HasMaxLength(45)
+                .HasColumnName("group");
+            entity.Property(e => e.Name)
+                .HasMaxLength(45)
+                .HasColumnName("name");
+            entity.Property(e => e.Patronymic)
+                .HasMaxLength(45)
+                .HasColumnName("patronymic");
+            entity.Property(e => e.Surname)
+                .HasMaxLength(45)
+                .HasColumnName("surname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
