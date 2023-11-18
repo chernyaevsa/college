eed1022a62b4db94a6dfeb3dcb3f895de40e92f2
csa <csa@czsa.ru>
2023-11-18 09:51:06 +0500

    Добавил контроллер Lessons

---
+++ API/Controllers/LessonsController.cs
@@ -0,0 +1,53 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("/lessons")]
+	public class LessonsController : ControllerBase
+	{
+		[HttpGet]
+		[Route("{id}")]
+		public IActionResult GetById(int id)
+		{
+			var db = new CollegeContext();
+			var lesson = db.Lessons.SingleOrDefault(s => s.Id == id);
+			if (lesson == null)
+				return NotFound();
+			return Ok(lesson);
+		}
+		[HttpGet]
+		public IActionResult GetAll()
+		{
+			var db = new CollegeContext();
+			return Ok(db.Lessons);
+		}
+		[HttpPost]
+		public IActionResult Add(Lesson lesson)
+		{
+			var db = new CollegeContext();
+			db.Lessons.Add(lesson);
+			db.SaveChanges();
+			return Ok(lesson);
+		}
+		[HttpPut]
+		public IActionResult Edit(Lesson lesson)
+		{
+			var db = new CollegeContext();
+			db.Lessons.Update(lesson);
+			db.SaveChanges();
+			return Ok(lesson);
+		}
+		[HttpDelete]
+		[Route("{id}")]
+		public IActionResult Delete(int id)
+		{
+			var db = new CollegeContext();
+			var lesson = db.Lessons.SingleOrDefault(s => s.Id == id);
+			if (lesson == null)
+				return NotFound();
+			return Ok(lesson);
+		}
+	}
+}
